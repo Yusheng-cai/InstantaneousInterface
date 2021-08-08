5a343911f8c5ce3375ff2c49f0d389fd06e6076a
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-07 21:37:50 -0400

    Added initialization to driver code

---
+++ src/Driver.cpp
@@ -0,0 +1,129 @@
+#include "Driver.h"
+
+Driver::Driver(const ParameterPack& pack, const CommandLineArguments& cmd)
+{
+    bool read = cmd.readString("abspath", CommandLineArguments::Keys::Optional, abs_path_);
+    if ( ! read)
+    {
+        abs_path_ = FileSystem::getCurrentPath();
+    }
+
+    // required xdr file input
+    auto xdrPack = pack.findParamPack("xdrfile",ParameterPack::KeyType::Required);
+    initializeXdrFile(xdrPack);
+
+    // find gro file input
+    auto groPack = pack.findParamPack("grofile", ParameterPack::KeyType::Optional);
+    if(groPack != nullptr)
+    {
+        initializeGroFile(groPack);
+    }
+
+    // find all the atomgroup packs
+    auto atomgroupPack = pack.findParamPacks("atomgroup", ParameterPack::KeyType::Required);
+    initializeAtomGroups(atomgroupPack);
+
+    // Find the bounding box
+    auto boundingboxPack = pack.findParamPacks("boundingbox", ParameterPack::KeyType::Required);
+    initializeBoundingBox(boundingboxPack);
+
+    // find the density field
+    auto DensityPack = pack.findParamPack("densityfield", ParameterPack::KeyType::Required);
+    initializeDensityField(DensityPack);
+}
+
+void Driver::initializeDensityField(const ParameterPack* densityPack)
+{
+    DensityFieldInput input = {simstate_,const_cast<ParameterPack&>(*densityPack)};
+
+    densityfield_ = DensityPtr(new DensityField(input));
+}
+
+void Driver::initializeBoundingBox(std::vector<const ParameterPack*>& bbPack)
+{
+    for (int i=0;i<bbPack.size();i++)
+    {
+        std::string bbName;
+        auto pack = bbPack[i];
+
+        pack -> ReadString("name", ParameterPack::KeyType::Required, bbName);
+        BoundingBoxInput input = {const_cast<ParameterPack&>(*pack), simstate_};
+
+        BoundingBox box(input);
+
+        simstate_.registerBoundingBox(bbName, box);
+    }
+}
+
+void Driver::update()
+{
+    xdrfile_ -> readNextFrame();
+
+    auto& positions_ = xdrfile_ -> getPositions();
+
+    for (int i=0;i<AtomGroupNames_.size();i++)
+    {
+        auto& agName = AtomGroupNames_[i];
+        auto& ag     = simstate_.getAtomGroup(agName);
+
+        ag.update(positions_); 
+    }
+}
+
+void Driver::initializeXdrFile(const ParameterPack* xdrPack)
+{
+    std::string xdrPath;
+    xdrPack -> ReadString("path", ParameterPack::KeyType::Required, xdrPath);
+
+    int found = xdrPath.find_last_of(".");
+    ASSERT((found != std::string::npos), "Somehow the . character is not found in the path, it is needed to specify\
+    what kind of file is being passed in.");
+
+    std::string type = xdrPath.substr(found+1);
+
+    XdrInput input = {const_cast<ParameterPack&>(*xdrPack), abs_path_};
+
+    xdrfile_ = XdrPtr(XdrFiles::factory::instance().create(type, input));
+
+    xdrfile_ -> open();
+}
+
+void Driver::initializeGroFile(const ParameterPack* groPack)
+{
+    groPack->ReadString("path", ParameterPack::KeyType::Required, groPath_);
+
+    grofile_.Open(groPath_);
+}
+
+void Driver::initializeAtomGroups(std::vector<const ParameterPack*>& agPack)
+{
+    for (int i=0;i<agPack.size();i++)
+    {
+        auto pack = agPack[i];
+        std::string atomName_;
+        pack -> ReadString("name", ParameterPack::KeyType::Required, atomName_);
+
+        AtomGroupInput input = {const_cast<ParameterPack&>(*pack), grofile_};
+
+        AtomGroup group(input);
+
+        simstate_.registerAtomGroup(atomName_, group);
+
+        AtomGroupNames_.push_back(atomName_);
+    }
+}
+
+const AtomGroup& Driver::getAtomGroup(const std::string& name) const 
+{
+    return simstate_.getAtomGroup(name);
+}
+
+AtomGroup& Driver::getAtomGroup(const std::string& name) 
+{
+    return simstate_.getAtomGroup(name);
+}
+
+void Driver::calculate()
+{
+
+}
--- src/Driver.h
+++ src/Driver.h
@@ -1,6 +1,12 @@
 #include "AtomGroup.h"
 #include "SimulationState.h"
 #include "DensityField.h"
+#include "xdr/XdrWrapper.h"
+#include "xdr/GroFile.h"
+#include "tools/InputParser.h"
+#include "tools/CommandLineArguments.h"
+#include "tools/FileSystem.h"
+#include "BoundingBox.h"
 
 #include <vector>
 #include <memory>
@@ -9,5 +15,51 @@
 class Driver
 {
     public:
+        using XdrPtr = std::unique_ptr<XdrWrapper>;
+        using DensityPtr = std::unique_ptr<DensityField>;
+        using BBPtr  = std::unique_ptr<BoundingBox>;
+        using Real3  = CommonTypes::Real3;
+
+        Driver(const ParameterPack& pack, const CommandLineArguments& cmd);
+
+        void initializeAtomGroups(std::vector<const ParameterPack*>& agPack);
+
+        void initializeGroFile(const ParameterPack* groPack);
+
+        void initializeXdrFile(const ParameterPack* xdrPack);
+
+        void initializeBoundingBox(std::vector<const ParameterPack*>& bbPack);
+
+        void initializeDensityField(const ParameterPack* densityPack);
+
+        void update();
+
+        void calculate();
+
+        // get num frames
+        int getNumFrames() const {return xdrfile_->getNframes();}
+
+        const std::vector<Real3> getPositions() const {return xdrfile_->getPositions();}
+        std::vector<Real3> accessPositions() {return xdrfile_->getPositions();}
+
+        const AtomGroup& getAtomGroup(const std::string& name) const; 
+        AtomGroup& getAtomGroup(const std::string& name);
+
     private:
+        SimulationState simstate_;
+        GroFile grofile_;
+
+        std::vector<std::string> AtomGroupNames_;
+        std::vector<std::string> BBNames_;
+
+        std::string groPath_;
+
+        XdrPtr xdrfile_;
+
+        // the absolute path of the program
+        std::string abs_path_;
+
+        DensityPtr densityfield_;
+
+        BBPtr boundingbox_;
 };
