851a4e72e622ac318b4b2986e63fe93b8c28ebe1
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-09 21:34:12 -0400

    Now calling more functions from driver in main and added timer

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <string>
+#include <chrono>
 
 int main(int argc, char** argv)
 {
@@ -16,6 +17,23 @@ int main(int argc, char** argv)
 
     Driver d(pack, cmd);
 
+    auto start = std::chrono::high_resolution_clock::now();
     d.update();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
+    std::cout << "Time it took for update is " << diff.count() << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
     d.calculate();
+    end = std::chrono::high_resolution_clock::now();
+    auto diff2 = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
+    std::cout << "Time it took for calculate is " << diff2.count() << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
+    d.finishCalculate();
+    end = std::chrono::high_resolution_clock::now();
+    auto diff3 = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
+    std::cout << "Time it took for finish calc is " << diff3.count() << std::endl;
+
+    d.printOutputfile();
 }
