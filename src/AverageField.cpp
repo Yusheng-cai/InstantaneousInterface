4e5796e59fdb111d3e462e273cb471ccdf1f6f2c
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-09 21:31:09 -0400

    Added function to precalculate density

--- src/AverageField.cpp
+++ src/AverageField.cpp
@@ -9,24 +9,83 @@ AverageField::AverageField(const DensityFieldInput& input)
 :DensityField(input)
 {}
 
+void AverageField::precalculateDensity()
+{
+    DensityPreCalculate_.clear(); 
+    DensityPreCalculate_.resize(offsetIndex_.size());
+
+    Real3 originPos = field_.getPositionOnGrid(0,0,0);
+
+    #pragma omp parallel for
+    for (int i=0;i<offsetIndex_.size();i++)
+    {
+        index3 index = offsetIndex_[i];
+
+        Real3 pos = field_.getPositionOnGrid(index[0], index[1], index[2]);
+
+        Real3 distance;
+        Real sq_dist;
+        simstate_.getSimulationBox().calculateDistance(pos, originPos, distance, sq_dist);
+
+        Real val = GaussianCoarseGrain::GaussianCoarseGrainFunction(distance, sigma_);
+        DensityPreCalculate_[i] = val;
+    }
+}
+
 void AverageField::calculate()
 {
+    precalculateDensity();
+
     auto atomgroup = getAtomGroup(atomGroupName_);
     auto& atoms = atomgroup.getAtoms();
-    field_.zero();
 
-    for (int i=0;i<atoms.size();i++)
+    // set up the omp buffers
+    FieldBuffer_.set_master_object(field_);
+    AtomIndicesInside_.clear();
+    AtomIndicesBuffer_.set_master_object(AtomIndicesInside_);
+
+    #pragma omp parallel
     {
-        if( bound_box_ -> isInside(atoms[i].position))
+        auto& indices_buffer = AtomIndicesBuffer_.access_buffer_by_id();
+        #pragma omp for
+        for(int i=0;i<atoms.size();i++)
         {
-            std::cout << "Position before correction = " << atoms[i].position[0] << " " << atoms[i].position[1] << " " << \
-            atoms[i].position[2] << std::endl;
+            if (bound_box_ -> isInside(atoms[i].position))
+            {
+                indices_buffer.push_back(i);
+            }
+        }
+    }
 
-            Real3 correctedPos = bound_box_->PutInBoundingBox(atoms[i].position);
+    int size = AtomIndicesInside_.size();
+    for (auto it = AtomIndicesBuffer_.beginworker();it != AtomIndicesBuffer_.endworker();it++)
+    {
+        size += it -> size();
+    }
 
-            std::cout << "Positon after correction = " << correctedPos[0] << " " << correctedPos[1] << " " << correctedPos[2] << std::endl;
-            index3 Index       = field_.getClosestGridIndex(correctedPos);
-            std::cout << "Closest position = " << Index[0] << " " << Index[1] << " " << Index[2] << std::endl;
+    AtomIndicesInside_.reserve(size);
+
+    for (auto it = AtomIndicesBuffer_.beginworker();it != AtomIndicesBuffer_.endworker();it++)
+    {
+        AtomIndicesInside_.insert(AtomIndicesInside_.end(), it->begin(), it -> end());
+    }
+
+    #pragma omp parallel
+    {
+        auto& fieldbuf = FieldBuffer_.access_buffer_by_id();
+
+        #pragma omp for simd
+        for (int i=0;i<AtomIndicesInside_.size();i++)
+        {
+            int indices = AtomIndicesInside_[i];
+            //std::cout << "Position before correction = " << atoms[indices].position[0] << " " << atoms[indices].position[1] << " " << \
+            atoms[indices].position[2] << std::endl;
+
+            Real3 correctedPos = bound_box_->PutInBoundingBox(atoms[indices].position);
+
+            //std::cout << "Positon after correction = " << correctedPos[0] << " " << correctedPos[1] << " " << correctedPos[2] << std::endl;
+            index3 Index       = fieldbuf.getClosestGridIndex(correctedPos);
+            //std::cout << "Closest position = " << Index[0] << " " << Index[1] << " " << Index[2] << std::endl;
 
             for(int j=0;j<offsetIndex_.size();j++)
             {
@@ -36,24 +95,66 @@ void AverageField::calculate()
                     RealIndex[k] = Index[k] + offsetIndex_[j][k];
                 }
 
-                std::cout << "Index = " << RealIndex[0] << " " << RealIndex[1] << " " << RealIndex[2] << std::endl;
-                Real3 latticepos = field_.getPositionOnGrid(RealIndex[0], RealIndex[1], RealIndex[2]);
-                std::cout << "Position on grid = " << latticepos[0] << " " << latticepos[1] << " " << latticepos[2] << std::endl;
+                // std::cout << "Index = " << RealIndex[0] << " " << RealIndex[1] << " " << RealIndex[2] << std::endl;
+                Real3 latticepos = fieldbuf.getPositionOnGrid(RealIndex[0], RealIndex[1], RealIndex[2]);
+                // std::cout << "Position on grid = " << latticepos[0] << " " << latticepos[1] << " " << latticepos[2] << std::endl;
 
-                Real3 diff;
-                diff.fill(0);
+                // Real3 diff;
+                // diff.fill(0);
 
-                for (int k=0;k<3;k++)
-                {
-                    diff[k] = std::abs(latticepos[k] - correctedPos[k]);
-                }
+                // for (int k=0;k<3;k++)
+                // {
+                //     diff[k] = std::abs(latticepos[k] - correctedPos[k]);
+                // }
 
-                Real val = GaussianCoarseGrain::GaussianCoarseGrainFunction(diff, sigma_);
+                // Real val = GaussianCoarseGrain::GaussianCoarseGrainFunction(diff, sigma_);
 
-                std::cout << "The value of the field = " << val << std::endl;
+                Real val = DensityPreCalculate_[j];
+                // std::cout << "val = " << val << std::endl;
+                // std::cout << "index = " << RealIndex[0] << " " << RealIndex[1] << " " << RealIndex[2] << std::endl;
 
-                field_(RealIndex[0], RealIndex[1], RealIndex[2]) += val;
+                // std::cout << "The value of the field = " << val << std::endl;
+
+                fieldbuf(RealIndex[0], RealIndex[1], RealIndex[2]) += val;
             }
+        }    
+    }
+
+    #pragma omp parallel for
+    for (int i=0;i<field_.totalSize();i++)
+    {
+        for (auto it = FieldBuffer_.beginworker();it != FieldBuffer_.endworker();it++)
+        {
+            field_.accessField()[i] += it->accessField()[i];
         }
-    }    
+    }
+}
+
+void AverageField::finishCalculate()
+{
+    MarchingCubes_.calculate(field_, vertices_, triangles_, isoSurfaceVal_);
+
+    // for (int i=0;i<vertices_.size();i++)
+    // {
+    //     for (int j=0;j<3;j++)
+    //     {
+    //         std::cout << vertices_[i].position_[j] << " ";
+    //     }
+    //     std::cout << "\n";
+    // }
+}
+
+void AverageField::printOutputIfOnStep()
+{
+    index3 N = field_.getN();
+    int Nx = N[0];
+    int Ny = N[1];
+    int Nz = N[2];
+
+    for (int i=0;i<field_.accessField().size();i++) 
+    {
+        ofs_ << field_.accessField()[i];
+
+        ofs_ << " ";
+    }
 }
--- src/AverageField.h
+++ src/AverageField.h
@@ -1,6 +1,10 @@
 #include "DensityField.h"
+#include "parallel/OpenMP_buffer.h"
 
 #include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
 
 class AverageField:public DensityField
 {
@@ -9,5 +13,15 @@ class AverageField:public DensityField
 
         virtual void calculate() override;
         virtual void update() {};
+        virtual void finishCalculate() override;
+        virtual void printOutputIfOnStep() override; 
+
+        void precalculateDensity();
+
     private:
+        std::vector<int> AtomIndicesInside_;
+        OpenMP::OpenMP_buffer<std::vector<int>> AtomIndicesBuffer_;
+        std::string outputFileName_;
+
+        std::vector<Real> DensityPreCalculate_;
 };
