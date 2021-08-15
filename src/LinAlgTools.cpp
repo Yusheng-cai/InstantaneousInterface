05ff881c8c3a4e69ea519037980f097258b1c323
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-15 18:11:56 -0400

    Removed some unecessary functions in linalgtools

--- src/LinAlgTools.cpp
+++ src/LinAlgTools.cpp
@@ -63,37 +63,18 @@ void LinAlg3x3::normalize(Real3& v1)
     }
 }
 
-bool LinAlg3x3::ldltdc(Matrix& A, Real3 rdiag)
+LinAlg3x3::Real3 LinAlg3x3::MatrixDotVector(const Matrix& A, const Real3& v1)
 {
-    // Special case for small N
-    Real d0 = A[0][0];
-    rdiag[0] = 1 / d0;
+    Real3 ans;
+    ans.fill(0);
 
-    A[1][0] = A[0][1];
-    Real l10 = rdiag[0] * A[1][0];
-    Real d1 = A[1][1] - l10 * A[1][0];
-    rdiag[1] = 1 / d1;
-
-    Real d2 = A[2][2] - rdiag[0] * std::pow(A[2][0],2.0) - rdiag[1] * std::pow(A[2][1],2.0);
-    rdiag[2] = 1 / d2;
-    A[2][0] = A[0][2];
-    A[2][1] = A[1][2] - l10 * A[2][0];
-
-    return (d0 != 0 && d1 != 0 && d2 != 0);
-}
-
-void LinAlg3x3::ldltsl(const Matrix& A, const Real3& rdiag, const Real3& b, Real3& x) 
-{
-    for (int i = 0; i < 3; i++) {
-        Real sum = b[i];
-        for (int k = 0; k < i; k++)
-            sum -= A[i][k] * x[k];
-        x[i] = sum * rdiag[i];
+    for (int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            ans[i] += A[i][j]*v1[j];
+        }
     }
-    for (int i = 3 - 1; i >= 0; i--) {
-        Real sum = 0;
-        for (int k = i + 1; k < 3; k++)
-            sum += A[k][i] * x[k];
-        x[i] -= sum * rdiag[i];
-    }
+
+    return ans;
 }
--- src/LinAlgTools.h
+++ src/LinAlgTools.h
@@ -1,5 +1,6 @@
 #pragma once
-#include <tools/CommonTypes.h>
+#include "tools/CommonTypes.h"
+#include "tools/Assert.h"
 
 #include <cmath>
 
@@ -15,14 +16,5 @@ namespace LinAlg3x3
     void normalize(Real3& v1);
     Matrix dyad(const Real3& v1, const Real3& v2);
 
-    // LDL^T decomposition of a symmetric positive definite matrix (and some
-    // other symmetric matrices, but fragile since we don't do pivoting).
-    // Like Cholesky, but no square roots, which is important for small N.
-    // Reads diagonal and upper triangle of matrix A.
-    // On output, lower triangle of A holds LD, while rdiag holds D^-1.
-    // Algorithm from Golub and van Loan.
-    bool ldltdc(Matrix& A, Real3 rdiag);
-
-    // Solve Ax=b after ldltdc.  x is allowed to be the same as b. 
-    void ldltsl(const Matrix& A, const Real3& rdiag, const Real3& b, Real3& x); 
+    Real3 MatrixDotVector(const Matrix& mat1, const Real3& v1); 
 }
