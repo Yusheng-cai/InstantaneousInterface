5559743d956bf5dde0dd3461be3269bdd5ef520f
Yusheng Cai <cys9741@seas.upenn.edu>
2021-08-16 16:08:30 -0400

    Added code for calculating curvature using curve fitting

---
+++ src/CurvatureCurveFit.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Graph.h"
+#include "Curvature.h"
+#include "tools/Assert.h"
+#include "tools/CommonTypes.h"
+#include "Graph.h"
+#include "LinAlgTools.h"
+
+#include <vector>
+#include <array>
+
+class CurvatureCurveFit : public Curvature
+{
+    public:
+        using Matrix = CommonTypes::Matrix;
+        using Real2  = CommonTypes::Real2;
+        CurvatureCurveFit(CurvatureInput& input);
+
+        virtual void calculate() override;
+        virtual void printOutput() override;
+    
+    private:
+        int NumParameters_ = 3;
+        int NumNeighbors_ = 1;
+
+        std::vector<Real2> CurvaturePerVertex_;
+};
