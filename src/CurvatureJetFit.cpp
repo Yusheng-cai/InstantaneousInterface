f0c3d49d588a0a67fab490a6de5c19be4b3e4af3
Yusheng Cai <cys9741@seas.upenn.edu>
2021-10-20 17:39:46 -0400

    Updated the jet fitting code

--- src/CurvatureJetFit.cpp
+++ src/CurvatureJetFit.cpp
@@ -8,24 +8,109 @@ namespace CurvatureRegistry
 CurvatureJetFit::CurvatureJetFit(CurvatureInput& input)
 :Curvature(input)
 {
+    input.pack.ReadNumber("neighbors", ParameterPack::KeyType::Required, numneighbors_);
+    input.pack.ReadNumber("degree", ParameterPack::KeyType::Optional, degree_);
+    input.pack.ReadNumber("MongeCoefficient", ParameterPack::KeyType::Optional, MongeCoefficient_);
+
+    outputs_.registerOutputFunc("neighborIndices", [this](std::string name) -> void {this -> printNeighbors(name);});
+    outputs_.registerOutputFunc("coefficients", [this](std::string name) -> void {this -> printCoefficientPerVertex(name);});
 }
 
 void CurvatureJetFit::calculate()
 {
-    auto& vertices = mesh_.accessvertices();
-    std::vector<Dpoint> vec_;
-    for (int i=0;i<vertices.size();i++)
+    mesh_.findVertexNeighbors();
+
+    const auto& VertexNeighbors_ = mesh_.getNeighborIndices();
+    const auto& vertices = mesh_.getvertices();
+    Graph::getNearbyIndicesNVertexAway(VertexNeighbors_, numneighbors_,NeighborIndicesNVertex_);
+    CurvaturePerVertex_.resize(vertices.size());
+    coefficientsPerVertex_.resize(vertices.size());
+
+    for (int i=0;i<NeighborIndicesNVertex_.size();i++)
     {
-        auto& pos = vertices[i].position_;
-        Dpoint point(pos[0], pos[1], pos[2]);
+        std::vector<Dpoint> vec_;
+        auto& thisVertex = vertices[i];
+        auto& thisPos = vertices[i].position_;
+        auto& thisNormal = thisVertex.normals_;
+        Dpoint point(thisPos[0], thisPos[1], thisPos[2]);
         vec_.push_back(point);
-    }
 
-    mform_ = jetfitter_(vec_.begin(), vec_.end(), 3, 2);
+        for (int j=0;j<NeighborIndicesNVertex_[i].size();j++)
+        {
+            int neighborIndex = NeighborIndicesNVertex_[i][j];
+            auto& vertpos = vertices[neighborIndex].position_;
 
-    for (int i=0;i<vertices.size();i++)
-    {
-        std::cout << "Curvature of " << i << " = " << mform_.principal_curvatures(i) << std::endl;
+            Dpoint point(vertpos[0], vertpos[1], vertpos[2]);
+            vec_.push_back(point);
+        }
+
+        mform_ = jetfitter_(vec_.begin(), vec_.end(), degree_, MongeCoefficient_);
+        DVector norm(thisNormal[0], thisNormal[1], thisNormal[2]);
+
+        mform_.comply_wrt_given_normal(norm);
+        std::cout << "Origin = " << mform_.origin() << std::endl;
+        std::cout << "Position1 = " << vec_[0] << std::endl;
+        std::cout << "vertex position = " << vertices[i].position_[0] << " " << vertices[i].position_[1] << " " << vertices[i].position_[2] << std::endl;
+
+        std::vector<Real> coeff_;
+
+        for (int i=0;i<mform_.coefficients().size();i++)
+        {
+            coeff_.push_back(mform_.coefficients()[i]);
+        }
+
+        coefficientsPerVertex_.push_back(coeff_);
+        CurvaturePerVertex_[i][0] = mform_.coefficients()[0];
+        CurvaturePerVertex_[i][1] = mform_.coefficients()[1];
     }
+    
     std::cout << "FINISHED." << std::endl;
+}
+
+
+void CurvatureJetFit::printCurvature(std::string name)
+{
+    std::ofstream ofs_;
+    ofs_.open(name);
+
+    ofs_ << "# k1 k2" << "\n";
+    for(int i=0;i<CurvaturePerVertex_.size();i++)
+    {
+        ofs_ << CurvaturePerVertex_[i][0] << " " << CurvaturePerVertex_[i][1] << "\n";
+    }
+    ofs_.close();
+}
+
+void CurvatureJetFit::printNeighbors(std::string name)
+{
+    std::ofstream ofs_;
+    ofs_.open(name);
+
+    for (int i=0;i<NeighborIndicesNVertex_.size();i++)
+    {
+        int neighborsize = NeighborIndicesNVertex_[i].size();
+        for (int j=0;j<neighborsize;j++)
+        {
+            ofs_ << NeighborIndicesNVertex_[i][j] << " ";
+        }
+        ofs_ << "\n";
+    }
+    ofs_.close();
+}
+
+void CurvatureJetFit::printCoefficientPerVertex(std::string name)
+{
+    std::ofstream ofs_;
+    ofs_.open(name);
+
+    for (int i=0;i<coefficientsPerVertex_.size();i++)
+    {
+        int sizePerVertex = coefficientsPerVertex_[i].size();
+        for (int j=0;j<sizePerVertex;j++)
+        {
+            ofs_ << coefficientsPerVertex_[i][j] << " ";
+        }
+        ofs_ << "\n";
+    }
+    ofs_.close();
 }
--- src/CurvatureJetFit.h
+++ src/CurvatureJetFit.h
@@ -2,6 +2,7 @@
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Monge_via_jet_fitting.h>
 #include "tools/CommonTypes.h"
+#include "Graph.h"
 #define CGAL_EIGEN3_ENABLED 1
 
 #include <vector>
@@ -13,11 +14,23 @@ class CurvatureJetFit : public Curvature
         using Dpoint     = DataKernel::Point_3;
         using MongeViaJetFitting = CGAL::Monge_via_jet_fitting<DataKernel>;
         using Mongeform =  MongeViaJetFitting::Monge_form;
-
+        using DVector    = DataKernel::Vector_3;
 
         CurvatureJetFit(CurvatureInput& input);
         virtual void calculate() override;
+
+        virtual void printCurvature(std::string name) override;
+        void printNeighbors(std::string name);
+        void printCoefficientPerVertex(std::string name);
+
     private:
         MongeViaJetFitting jetfitter_;
         Mongeform mform_;
+        int numneighbors_;
+        int degree_=2;
+        int MongeCoefficient_=2;
+
+        std::vector<Real2> CurvaturePerVertex_;
+        std::vector<std::vector<int>> NeighborIndicesNVertex_;
+        std::vector<std::vector<Real>> coefficientsPerVertex_;
 };
