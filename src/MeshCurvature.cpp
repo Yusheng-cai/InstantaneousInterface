dceb82ec803403ad8b443936b3929ebd862af7ab
Yusheng Cai <cys9741@seas.upenn.edu>
2022-01-27 20:14:52 -0500

    Updated codes for considering boundary condition for curvature updates as well as some minor changes

--- src/MeshCurvature.cpp
+++ src/MeshCurvature.cpp
@@ -19,31 +19,129 @@ MeshCurvature::MeshCurvature(MeshRefineStrategyInput& input)
     pack_.ReadNumber("tolerance", ParameterPack::KeyType::Optional, tol_);
     pack_.ReadNumber("k0", ParameterPack::KeyType::Required, meanCurvature_);
     pack_.ReadNumber("maxstep", ParameterPack::KeyType::Optional, maxStep);
+    pack_.Readbool("IgnoreOppositeSignCurvature", ParameterPack::KeyType::Optional,ignoreOppositeSign_);
+    pack_.Readbool("FixBoundary", ParameterPack::KeyType::Optional,fixBoundary_);
+    pack_.ReadString("errorfile", ParameterPack::KeyType::Optional, errorFile_);
+    pack_.ReadNumber("skip", ParameterPack::KeyType::Optional, skip_);
+
+    // initialize the file to output the error
+    initializeErrorFile();
 
     // read in xtc info
     xtcWrite_ = pack_.ReadString("xtcfile", ParameterPack::KeyType::Optional, xtcName);
 
     if (xtcWrite_)
     {
-        pack_.ReadNumber("xtcskip", ParameterPack::KeyType::Optional, skip_);
+        pack_.ReadNumber("xtcskip", ParameterPack::KeyType::Optional, xtcskip_);
         xtcFile_ = xtcptr(new XtcFile(xtcName, "write"));
         xtcFile_->open();
     }
 }
 
+void MeshCurvature::initializeErrorFile()
+{
+    if (! errorFile_.empty())
+    {
+        errorStream_.open(errorFile_);
+    }
+}
+
+void MeshCurvature::printToErrorFile()
+{
+    if (! errorFile_.empty())
+    {
+        if ((iteration_ % skip_) == 0 )
+        {
+            errorStream_ << iteration_ << " " << err_ << "\n";
+        }
+    }
+}
+
+void MeshCurvature::findVertices()
+{
+    // first let's see if user has provided with information about bounding boxes 
+    Real2 xrange, yrange, zrange;
+    Real INF = std::numeric_limits<Real>::infinity();
+
+    xrange = {{-INF, INF}};
+    yrange = {{-INF, INF}};
+    zrange = {{-INF, INF}};
+
+    bool xrangeRead = pack_.ReadArrayNumber("xrange", ParameterPack::KeyType::Optional, xrange);
+    bool yrangeRead = pack_.ReadArrayNumber("yrange", ParameterPack::KeyType::Optional, yrange);
+    bool zrangeRead = pack_.ReadArrayNumber("zrange", ParameterPack::KeyType::Optional, zrange);
+
+    const auto& vertices = mesh_.getvertices();
+
+    // If none of these are provided, then we keep the boundary vertices constant
+    if (fixBoundary_)
+    {
+        mesh_.MapEdgeToFaces();
+        mesh_.findBoundaryVertices();
+
+        for (int i=0;i<vertices.size();i++)
+        {
+            if (! mesh_.isBoundary(i))
+            {
+                OutsideIndices_.push_back(i);
+            }
+        }
+    }
+
+    if (xrangeRead || yrangeRead || zrangeRead)
+    {
+        const auto& vertices = mesh_.getvertices();
+
+        for (int i=0;i<vertices.size();i++)
+        {
+            auto v = vertices[i];
+            bool inX = (v.position_[0] > xrange[0]) && (v.position_[0] < xrange[1]);
+            bool inY = (v.position_[1] > yrange[0]) && (v.position_[1] < yrange[1]);
+            bool inZ = (v.position_[2] > zrange[0]) && (v.position_[2] < zrange[1]);
+
+            // if not all of them are inside the thing
+            if (! (inX && inY && inZ))
+            {
+                OutsideIndices_.push_back(i);
+            }
+        }
+    }
+
+    if (ignoreOppositeSign_)
+    {
+        // calculate one round of curvature 
+        curvatureCalc_ -> calculate(mesh_);
+        const auto& avgCurvature = curvatureCalc_->getAvgCurvaturePerVertex();
+
+        std::vector<int> temp;
+        for (auto ind: OutsideIndices_)
+        {
+            if (std::signbit(avgCurvature[ind]) == std::signbit(meanCurvature_))
+            {
+                temp.push_back(ind);
+            }
+        }
+
+        OutsideIndices_.insert(OutsideIndices_.end(), temp.begin(), temp.end());
+    }
+
+    // Find the unique indices 
+    auto ip = std::unique(OutsideIndices_.begin(), OutsideIndices_.end());
+    OutsideIndices_.resize(std::distance(OutsideIndices_.begin(), ip));
+
+    ASSERT((OutsideIndices_.size() <= vertices.size()), "The outside indices size = " << OutsideIndices_.size() << " while vertices size = " << vertices.size());
+}
+
 void MeshCurvature::refine()
 {
-    // First find the map from edges to faces 
-    mesh_.MapEdgeToFaces();
-
-    // From the above map, find boundary vertices 
-    mesh_.findBoundaryVertices();
+    // find the vertices to be updated
+    findVertices();
 
     // Start refining 
-    Real err = 10000000;
-    int iterations=0;
+    err_ = 10000000;
+    iteration_=0;
 
-    while (err >= tol_)
+    while (err_ >= tol_)
     {
         Real maxerr = -100000;
 
@@ -57,26 +155,28 @@ void MeshCurvature::refine()
         auto& vertices = mesh_.accessvertices();
 
         // update the vertices 
-        std::cout << "Iteration " << iterations << std::endl;
+        std::cout << "Iteration " << iteration_ << std::endl;
+        Real avgE=0.0;
         #pragma omp parallel
         {
             Real e=maxerr;
+            Real avgElocal = 0.0;
             #pragma omp for
-            for (int j=0;j<vertices.size();j++)
+            for (int j=0;j<OutsideIndices_.size();j++)
             {
-                if ( ! mesh_.isBoundary(j))
+                int index = OutsideIndices_[j];
+                Real diffkappa = meanCurvature_ - curvatures[index];
+ 
+                avgElocal += std::abs(diffkappa);
+
+                if (std::abs(diffkappa) > e)
                 {
-                    Real diffkappa = meanCurvature_ - curvatures[j];
+                    e = std::abs(diffkappa);
+                }
 
-                    if (std::abs(diffkappa/curvatures[j]) > e)
-                    {
-                        e = std::abs(diffkappa/meanCurvature_);
-                    }
-
-                    for (int k=0;k<3;k++)
-                    {
-                        vertices[j].position_[k] += StepSize_ * vertices[j].normals_[k] * diffkappa;
-                    }
+                for (int k=0;k<3;k++)
+                {
+                    vertices[index].position_[k] += StepSize_ * vertices[index].normals_[k] * diffkappa;
                 }
             }
 
@@ -85,33 +185,47 @@ void MeshCurvature::refine()
             {
                 maxerr = e;
             }
+
+            #pragma omp critical
+            {
+                avgE += avgElocal;
+            }
         }
 
-        std::cout << "Max error is " << maxerr << std::endl;
-        err = maxerr;
+        avgE = avgE / vertices.size();
+        err_ = maxerr / meanCurvature_;
+
+        std::cout << "Max error is " << err_ << std::endl;
+        std::cout << "average error is " << avgE << "\n";
 
         // update the normals as well 
         mesh_.CalcVertexNormals();
 
-        std::vector<Real3> vertPos(vertices.size());
-        for (int j=0;j<vertices.size();j++)
-        {
-            vertPos[j] = vertices[j].position_;
-        }
+        // update the iterations
+        iteration_ ++;
 
-        iterations ++;
-
+        // whether or not to write the xtc file
         if (xtcWrite_)
         {
-            if (iterations % skip_ == 0)
+            std::vector<Real3> vertPos(vertices.size());
+            for (int j=0;j<vertices.size();j++)
             {
-                xtcFile_->writeFrame(vertPos, xtcstep_, iterations,box);
+                vertPos[j] = vertices[j].position_;
+            }
+
+            if (iteration_ % xtcskip_ == 0)
+            {
+                xtcFile_->writeFrame(vertPos, xtcstep_, iteration_,box);
 
                 xtcstep_ ++;
             }
         }
 
-        if (iterations > maxStep)
+        // see if we need to record the error --> Max error
+        printToErrorFile();
+
+        // break the calculation if iteration is already maxed out
+        if (iteration_ > maxStep)
         {
             break;
         }
--- src/MeshCurvature.h
+++ src/MeshCurvature.h
@@ -20,25 +20,51 @@ class MeshCurvature : public MeshRefineStrategy
         using curveptr = std::unique_ptr<Curvature>;
         using xtcptr   = std::unique_ptr<XtcFile>;
         using Matrix   = CommonTypes::Matrix;
+        using Real2    = CommonTypes::Real2;
 
         MeshCurvature(MeshRefineStrategyInput& input);
 
         virtual void refine() override;
+
+        void findVertices();
+
+        void initializeErrorFile();
+
+        void printToErrorFile();
+
     private:
         std::string curvaturetype_;
         curveptr curvatureCalc_;
+
+        // The indices for the vertices to be kept constant 
+        std::vector<int> VertexIndices_;
+
+        // The indices for the outside vertices to not be kept constant 
+        std::vector<int> OutsideIndices_;
 
         Real StepSize_;
         Real meanCurvature_;
 
         Real tol_ = 1e-4;
 
+        // keep track of number of iteration
+        int iteration_=0;
+
         std::string xtcName="test.xtc";
         bool xtcWrite_=false;
         xtcptr xtcFile_;
         int xtcstep_=1;
-        int skip_=100;
+        int xtcskip_=100;
         int maxStep = 1e6;
+        bool ignoreOppositeSign_=false;
+        bool fixBoundary_=true;
+
+        // whether or not we want to print the error 
+        std::string errorFile_;
+        std::vector<Real> error_;
+        Real err_;
+        std::ofstream errorStream_;
+        int skip_=1;
 
         Matrix box={{{10,0,0},{0,10,0},{0,0,10}}};
 };
