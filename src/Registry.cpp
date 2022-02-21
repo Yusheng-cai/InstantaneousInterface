3afd1a2673f58cb5f9dd6c91522b31474770669e
Yusheng Cai <cys9741@seas.upenn.edu>
2022-02-21 00:11:07 -0500

    register mesh refinement

--- src/Registry.cpp
+++ src/Registry.cpp
@@ -16,3 +16,20 @@ Curvature& Registry::getCurvature(std::string name)
 
     return *(it->second);
 }
+
+void Registry::registerMeshRefinement(std::string name, const MeshRefineStrategy& refineStrat)
+{
+    auto it = MapNameToMeshRefinement_.find(name);
+    ASSERT((it == MapNameToMeshRefinement_.end()), "The name for mesh refinement " << name << " is already registered.");
+
+    MapNameToMeshRefinement_.insert(std::make_pair(name, const_cast<MeshRefineStrategy*>(&refineStrat)));
+}
+
+MeshRefineStrategy& Registry::getMeshRefineStrat(std::string name)
+{
+    auto it = MapNameToMeshRefinement_.find(name);
+
+    ASSERT((it != MapNameToMeshRefinement_.end()), "The name for mesh refinement " << name << " is not registered.");
+
+    return *(it->second);
+}
--- src/Registry.h
+++ src/Registry.h
@@ -2,6 +2,7 @@
 
 #include "tools/Assert.h"
 #include "Curvature.h"
+#include "MeshRefineStrategy.h"
 
 #include <string>
 #include <map>
@@ -15,6 +16,10 @@ class Registry
         void registerCurvature(std::string name, const Curvature& curve);
         Curvature& getCurvature(std::string name);
 
+        void registerMeshRefinement(std::string name, const MeshRefineStrategy& meshrefine);
+        MeshRefineStrategy& getMeshRefineStrat(std::string name);
+
     private:
         std::map<std::string, Curvature*> MapNameToCurvature_;
+        std::map<std::string, MeshRefineStrategy*> MapNameToMeshRefinement_;
 };
