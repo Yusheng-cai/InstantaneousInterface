efe6968daf74b7d3e331323d6ce9a7a11fedfa81
Yusheng Cai <cys9741@seas.upenn.edu>
2022-04-29 22:48:42 -0400

    Added new files to curvature flow

--- test/TODOtest/interface/plot.m
+++
@@ -1,9 +0,0 @@
-%% read ply 
-tri = pcread("zac.ply");
-pos = tri.Location;
-curve  = readtable("c.dat");
-meanC = curve.Average;
-scatter3(pos(1:end,1),pos(1:end,2), pos(1:end,3), 100,abs(meanC),"filled");
-colormap jet;
-colorbar;
-caxis([0,1.5]);
