7ad78829b7bc20a958bc055185c9e89ce391e229
Yusheng Cai <cys9741@seas.upenn.edu>
2022-08-29 17:13:33 -0400

    Added code for algorithm

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 int main(int argc, char** argv)
 {
     CommandLineArguments cmd(argc, argv);
+
+    // initialize the driver 
     InputParser ip;
     std::string fname = argv[1];
 
@@ -16,31 +18,5 @@ int main(int argc, char** argv)
     ip.ParseFile(fname,pack);
 
     Driver d(pack, cmd);
-
-    for (int i=0;i<d.getNumFrames();i++)
-    {
-        if (d.CheckValidStep(i))
-        {
-            std::cout << "Frame = " << i << std::endl;
-            d.readFrameXdr(i);
-
-            d.update();
-
-            auto start = std::chrono::high_resolution_clock::now();
-
-            d.calculate();
-
-            d.printOutputfileIfOnStep();
-            auto end = std::chrono::high_resolution_clock::now();
-            auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
-            std::cout << "Time it took for calculate is " << diff.count() << " milliseconds." << std::endl;
-        }
-    }
-    auto start = std::chrono::high_resolution_clock::now();
-    d.finishCalculate();
-    auto end = std::chrono::high_resolution_clock::now();
-    auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
-    std::cout << "Time it took for finish calculate is " << diff.count() << std::endl;
-
-    d.printFinalOutput();
+    d.run();
 }
---
+++ tools/Algorithm.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <random>
+
+namespace Algorithm
+{
+    void Permutation(int max, int numSamples, int numTimes, std::vector<std::vector<int>>& samples);
+};
