4b197368ec8f360dd8c487f7f6343531819fd579
Yusheng Cai <cys9741@seas.upenn.edu>
2022-08-30 17:29:02 -0400

    added templated function arange

--- tools/Algorithm.h
+++ tools/Algorithm.h
@@ -8,5 +8,16 @@
 namespace Algorithm
 {
     void Permutation(int max, int numSamples, int numTimes, std::vector<std::vector<int>>& samples);
-    void Permulation(int max, int numSamples, std::vector<int>& samples);
+    void Permutation(int max, int numSamples, std::vector<int>& samples);
+
+    template <typename T> 
+    std::vector<T> arange(T min, T max, T step);
 };
+
+template<typename T>
+std::vector<T> Algorithm::arange(T start, T stop, T step) {
+    std::vector<T> values;
+    for (T value = start; value < stop; value += step)
+        values.push_back(value);
+    return values;
+}
