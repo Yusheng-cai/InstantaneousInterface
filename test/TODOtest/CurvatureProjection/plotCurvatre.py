dc8c68501503faaab906f65bba028a69688278d7
Yusheng Cai <cys9741@seas.upenn.edu>
2022-09-08 15:34:36 -0400

    Added test for curvature projection

--- test/TODOtest/CurvatureProjection/plotCurvatre.py
+++ test/TODOtest/CurvatureProjection/plotCurvatre.py
@@ -19,23 +19,25 @@ if __name__ == "__main__":
         index1, index2 = int(d[0]), int(d[1])
         c[index1,index2] = d[-1]
 
-    dataP = read_data("hit.out")
-    p = []
-    for d in dataP:
-        if d[-1] == 1:
-            p.append([d[0],d[1]])
-    p = np.array(p)
-
-
     fig = plt.figure(dpi=300)
     ax  = fig.add_subplot(111)
-    levels = np.linspace(0,0.5,100)
     p = cm.ScalarMappable(cmap=cm.jet)
     p.set_array([0,0.5])
     p.set_clim(0,0.5)
     colors = p.to_rgba(c.T)
-    print(colors[0,0])
     ax.imshow(colors, interpolation="nearest")
+    fig.colorbar(p)
     plt.savefig("test.png")
 
+    # plot normal map
+    norm = read_data("normalMap.out")
+    normMap = np.zeros((1000,1000,4))
+    for d in norm:
+        index1,index2 = int(d[0]), int(d[1])
+        normMap[index2,index1] = np.array([d[2],d[3],d[4],1])
 
+    fig = plt.figure(dpi=300)
+    ax  = fig.add_subplot(111)
+    p   = ax.imshow(normMap, interpolation="none")
+    fig.colorbar(p)
+    plt.savefig("a.png")
