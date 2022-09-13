ba91cc2118984567562dbca0e8192896856cf255
Yusheng Cai <cys9741@seas.upenn.edu>
2022-09-13 15:25:51 -0400

    Changed grofile such that we can now read more than 1e6 atoms

--- xdr/GroFile.cpp
+++ xdr/GroFile.cpp
@@ -52,63 +52,50 @@ void GroFile::ParseFile()
     atomsinfo_.clear();
     long long minAtomNumber = 1000000000;
     std::set<int> ResidueSet;
+    int AtomNumber=1;
 
     for (int i=0; i< lines_.size() ;i++)
     {
         std::string sentence = lines_[i];
-        ASSERT((! sentence.empty()), "The sentence read is empty.");
 
-        int residueNumber;
-        std::string residueName;
-        std::string atomName;
-        int atomNumber;
-
-        // residue Number is 5 characters long
-        std::string residueNumberstr = sentence.substr(0,5);
-        // residue Name is 5 characters long
-        residueName = sentence.substr(5,5);
-        // atomName is 5 characters long
-        atomName    = sentence.substr(10,5);
-        // atom Number is 5 characters long
-        std::string atomNumberstr  = sentence.substr(15, 5);
-
-        // remove the blank spaces from the string of atomName and ResidueName
-        StringTools::RemoveBlankInString(residueName);
-        StringTools::RemoveBlankInString(atomName);
-        StringTools::RemoveBlankInString(atomName);
-        StringTools::RemoveBlankInString(atomNumberstr);
-
-        // convert the Numbers to int from string
-        residueNumber = StringTools::StringToType<int>(residueNumberstr);
-        atomNumber  = StringTools::StringToType<int>(atomNumberstr);
-
-        // instantiate the Atom object and append it to atomsinfo 
-        xdr::Atom a = {residueNumber, residueName, atomName, atomNumber};
-        atomsinfo_.push_back(std::move(a));
-        ResidueSet.insert(residueNumber);
-        AtomTypes_.insert(atomName);
-        ResidueNames_.insert(residueName);
-
-        if (atomNumber < minAtomNumber)
+        if (! sentence.empty())
         {
-            minAtomNumber = atomNumber;
+            int residueNumber;
+            std::string residueName;
+            std::string atomName;
+            int atomNumber;
+
+            // residue Number is 5 characters long
+            std::string residueNumberstr = sentence.substr(0,5);
+            // residue Name is 5 characters long
+            residueName = sentence.substr(5,5);
+            // atomName is 5 characters long
+            atomName    = sentence.substr(10,5);
+            // atom Number is 5 characters long
+            std::string atomNumberstr  = sentence.substr(15, 5);
+
+            // remove the blank spaces from the string of atomName and ResidueName
+            StringTools::RemoveBlankInString(residueName);
+            StringTools::RemoveBlankInString(atomName);
+            StringTools::RemoveBlankInString(atomName);
+            StringTools::RemoveBlankInString(atomNumberstr);
+
+            // convert the Numbers to int from string
+            residueNumber = StringTools::StringToType<int>(residueNumberstr);
+            atomNumber  = StringTools::StringToType<int>(atomNumberstr);
+
+            // instantiate the Atom object and append it to atomsinfo 
+            xdr::Atom a = {residueNumber, residueName, atomName, AtomNumber};
+            AtomNumber++;
+            atomsinfo_.push_back(std::move(a));
+            ResidueSet.insert(residueNumber);
+            AtomTypes_.insert(atomName);
+            ResidueNames_.insert(residueName);
         }
     }
 
     numResidues_ = ResidueSet.size();
     numUniqueResidues_ = ResidueNames_.size();
-
-    // correct for atom Number, we make it such that it always starts with 1 
-    if (minAtomNumber != 1)
-    {
-        int diff = 1 - minAtomNumber;
-
-        for (int i=0;i<atomsinfo_.size();i++)
-        {
-            auto atom = atomsinfo_[i];
-            atom.atomNumber_ += diff;
-        }
-    }
 
     // Check for the minimum of the residue number if residue Set is not empty
     if ( ! ResidueSet.empty())
