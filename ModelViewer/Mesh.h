e77ed8473401380e074d917411ca6505560b211f
Yusheng Cai <cys9741@seas.upenn.edu>
2022-09-21 12:00:50 -0400

    Updated modelviewer

---
+++ ModelViewer/Mesh.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "glad.h"
+#include <GLFW/glfw3.h>
+#include <string>
+#include <iostream>
+
+namespace ModelViewer{
+    class Mesh
+    {
+        public:
+            Mesh();
+        private:
+
+    };
+
+};
