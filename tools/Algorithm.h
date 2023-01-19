d9accbd78d2e1600c5e2f810190492d982eb83d9
Yusheng Cai <cys9741@seas.upenn.edu>
2023-01-19 12:54:12 -0500

    Updated algorithm code to include enumerate

--- tools/Algorithm.h
+++ tools/Algorithm.h
@@ -75,6 +75,9 @@ namespace Algorithm
     // insert something into vector map 
     template <typename key, typename value>
     void InsertInVectorMap(std::map<key,std::vector<value>>& map, key k, value v);
+
+    template <typename T, typename TIter=decltype(std::begin(std::declval<T>())), typename = decltype(std::end(std::declval<T>()))>
+    constexpr auto enumerate(T && iterable);
 };
 
 template<typename T>
@@ -253,4 +256,26 @@ void Algorithm::InsertInVectorMap(std::map<key, std::vector<value>>& map, key k,
 template <typename T>
 void Algorithm::sort(std::vector<T>& vec){
     std::sort(vec.begin(), vec.end());
+}
+
+
+template <typename T,
+          typename TIter = decltype(std::begin(std::declval<T>())),typename = decltype(std::end(std::declval<T>()))>
+constexpr auto Algorithm::enumerate(T && iterable)
+{
+    struct iterator
+    {
+        size_t i;
+        TIter iter;
+        bool operator != (const iterator & other) const { return iter != other.iter; }
+        void operator ++ () { ++i; ++iter; }
+        auto operator * () const { return std::tie(i, *iter); }
+    };
+    struct iterable_wrapper
+    {
+        T iterable;
+        auto begin() { return iterator{ 0, std::begin(iterable) }; }
+        auto end() { return iterator{ 0, std::end(iterable) }; }
+    };
+    return iterable_wrapper{ std::forward<T>(iterable) };
 }
